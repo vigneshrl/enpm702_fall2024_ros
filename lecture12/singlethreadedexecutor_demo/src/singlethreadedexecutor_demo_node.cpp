021a08fc07b1d6c5bcc0fe2b741dec32fda170c6
Adon <adon.sf4@gmail.com>
2024-11-19 13:44:08 -0500

    added delays to singlethreadedexecutor

--- lecture12/singlethreadedexecutor_demo/src/singlethreadedexecutor_demo_node.cpp
+++ lecture12/singlethreadedexecutor_demo/src/singlethreadedexecutor_demo_node.cpp
@@ -3,20 +3,24 @@
 
 void SingleThreadedExecutorDemoNodeOne::timer1()
 {
+    // rclcpp::sleep_for(std::chrono::seconds(5));
     RCLCPP_INFO_STREAM(this->get_logger(), output_green("node1 timer1"));
 }
 
 void SingleThreadedExecutorDemoNodeOne::timer2()
 {
+    // rclcpp::sleep_for(std::chrono::seconds(5));
     RCLCPP_INFO_STREAM(this->get_logger(), output_green("node1 timer2"));
 }
 
 void SingleThreadedExecutorDemoNodeTwo::timer1()
 {
+    // rclcpp::sleep_for(std::chrono::seconds(5));
     RCLCPP_INFO_STREAM(this->get_logger(), output_red("node2 timer1"));
 }
 
 void SingleThreadedExecutorDemoNodeTwo::timer2()
 {
+    // rclcpp::sleep_for(std::chrono::seconds(5));
     RCLCPP_INFO_STREAM(this->get_logger(), output_red("node2 timer2"));
 }
