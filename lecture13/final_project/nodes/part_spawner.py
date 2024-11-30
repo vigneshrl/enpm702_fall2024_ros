664ca2275172b541bb915239c02a6266fc433018
zeid <zeid.kootbally@nist.gov>
2024-11-30 15:39:57 -0500

    edited the world file

--- lecture13/final_project/nodes/part_spawner.py
+++ lecture13/final_project/nodes/part_spawner.py
@@ -44,32 +44,111 @@ if __name__ == "__main__":
 
     part_spawner = PartSpawner()
     part_spawner.spawn_part(
-        "blue_battery_99",
+        "blue_battery_1",
         "battery",
         "blue",
-        [-0.979630, 0.262296, 1],
+        [0.831388, 4.142605, 1.5],
         [0, 0, 0.77],
     )
 
     part_spawner.spawn_part(
-        "green_battery_99", "battery", "green", [4.039209, 2.856321, 1.0], [0, 0, 1.57]
+        "red_regulator_1",
+        "regulator",
+        "red",
+        [0.254164, 4.737885, 1.5],
+        [0, 0, 0.77],
     )
 
     part_spawner.spawn_part(
-        "red_battery_99", "battery", "red", [-3.941751, 8.195021, 1], [0, 0, -1.57]
+        "green_battery_1", "battery", "green", [5.0, 5.0, 1.5], [0, 0, 1.57]
+    )
+
+    part_spawner.spawn_part(
+        "red_battery_1", "battery", "red", [5.0, 0.0, 1.5], [0, 0, -1.57]
     )
     part_spawner.spawn_part(
-        "orange_battery_99",
+        "orange_battery_1",
         "battery",
         "orange",
-        [-9.752331, -0.288182, 1],
+        [5.0, -5.0, 1.0],
         [0, 0, 0],
     )
     part_spawner.spawn_part(
-        "purple_battery_99",
+        "green_pump_2",
+        "pump",
+        "green",
+        [3.806237, -5.0, 1.444065],
+        [0, 0, 0],
+    )
+    part_spawner.spawn_part(
+        "purple_battery_1",
         "battery",
         "purple",
-        [-0.872918, -7.414839, 1],
+        [0.392852, -4.231281, 1.0],
+        [0, 0.0, -0.78539816339],
+    )
+
+    part_spawner.spawn_part(
+        "red_pump_1",
+        "pump",
+        "red",
+        [0.0, -5.0, 1.0],
+        [0, 0.0, -0.78539816339],
+    )
+
+    part_spawner.spawn_part(
+        "orange_regulator_1",
+        "regulator",
+        "orange",
+        [-0.526753, -5.526753, 1.0],
+        [0, 0.0, -0.78539816339],
+    )
+
+    part_spawner.spawn_part(
+        "blue_battery_2",
+        "battery",
+        "blue",
+        [-4.753633, -5.891300, 1.171726],
+        [0, 0.0, -0.78539816339],
+    )
+
+    part_spawner.spawn_part(
+        "blue_battery_3",
+        "battery",
+        "blue",
+        [-4.438945, -4.839310, 1.171726],
+        [0, 0.0, -0.78539816339],
+    )
+
+    part_spawner.spawn_part(
+        "red_battery_2",
+        "battery",
+        "red",
+        [-4.628356, -0.371644, 1.0],
+        [0, 0.0, -0.78539816339],
+    )
+
+    part_spawner.spawn_part(
+        "green_pump_1",
+        "pump",
+        "green",
+        [-5.285342, 0.285342, 1.0],
+        [0, 0.0, -0.78539816339],
+    )
+
+    part_spawner.spawn_part(
+        "red_battery_3",
+        "battery",
+        "red",
+        [-4.642777, 5.357223, 1],
+        [0, 0.0, -0.78539816339],
+    )
+
+    part_spawner.spawn_part(
+        "purple_regulator_1",
+        "regulator",
+        "purple",
+        [-5.420137, 4.685093, 1],
         [0, 0.0, -0.78539816339],
     )
 
