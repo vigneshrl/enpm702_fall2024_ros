df3dd1429e3a1303fe1f7d844af2d1071fb403c2
Adon <adon.sf4@gmail.com>
2024-12-01 00:15:52 -0500

    modified launch files

--- lecture13/sensor_demo/launch/demo5.launch.py
+++ lecture13/sensor_demo/launch/demo5.launch.py
@@ -6,10 +6,13 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     # Grouped nodes
-    sensor_group = GroupAction([
+    navigation_group = GroupAction([
         Node(package='sensor_demo', executable='lidar_demo'),
         Node(package='sensor_demo', executable='camera_demo')
     ])
+    
+    temperature_node = Node(package='sensor_demo', executable='temperature_demo')
 
-    ld.add_action(sensor_group)
+    ld.add_action(navigation_group)
+    ld.add_action(temperature_node)
     return ld
